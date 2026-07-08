8da5ebe55e1058713fde5ac6c2e5e7c5b234a24c
Krishnandan2005 <krishnandannitb@gmail.com>
2026-07-08 18:04:42 +0530

    fixed a bug

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -3,39 +3,39 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const createCheckoutSession = async (req, res) => {
-    try {
-        const { amount } = req.body;
+  try {
+    const { amount } = req.body;
 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
 
-            line_items: [
-                {
-                    price_data: {
-                        currency: "inr",
-                        product_data: {
-                            name: "Flipkart Order",
-                        },
-                        unit_amount: amount * 100,
-                    },
-                    quantity: 1,
-                },
-            ],
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: "Flipkart Order",
+            },
+            unit_amount: amount * 100,
+          },
+          quantity: 1,
+        },
+      ],
 
-            mode: "payment",
+      mode: "payment",
 
-            success_url: "http://localhost:5173/cart",
-            cancel_url: "http://localhost:5173/cart",
-        });
+      success_url: "https://e-commerce-client-coral-sigma.vercel.app/cart",
+      cancel_url: "https://e-commerce-client-coral-sigma.vercel.app/cart",
+    });
 
-        res.json({
-            id: session.id,
-        });
-    } catch (error) {
-        console.log(error);
+    res.json({
+      id: session.id,
+    });
+  } catch (error) {
+    console.log(error);
 
-        res.status(500).json({
-            message: error.message,
-        });
-    }
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
